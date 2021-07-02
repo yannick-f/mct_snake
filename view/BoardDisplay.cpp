4c5fbaf17a416916af6a35495a12ebbf083e8444
Yannick Freischlad <freischladyannick@gmail.com>
2021-07-02 15:25:12 +0200

    init for BoardDisplay

--- view/BoardDisplay.cpp
+++ view/BoardDisplay.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "BoardDisplay.h"
+#include "gpio_msp432.h"
+#include "spi_msp432.h"
+#include "st7735s_drv.h"
+#include "uGUI.h"
+#include "uGUI_colors.h"
+
+void BoardDisplay::init() {
+	// Setup SPI interface
+	gpio_msp432_pin lcd_cs(PORT_PIN(5, 0));
+	spi_msp432 spi(EUSCI_B0_SPI, lcd_cs);
+	spi.setSpeed(24000000);
+
+	// Setup LCD driver
+	gpio_msp432_pin lcd_rst(PORT_PIN(5, 7));
+	gpio_msp432_pin lcd_dc(PORT_PIN(3, 7));
+	st7735s_drv lcd(spi, lcd_rst, lcd_dc, st7735s_drv::Crystalfontz_128x128);
+
+	// Setup uGUI
+	uGUI gui(lcd);
+
+	lcd.clearScreen(0x0);
+}
+;
--- view/BoardDisplay.h
+++ view/BoardDisplay.h
@@ -5,6 +5,14 @@
 #ifndef MCT_SNAKE_BOARDDISPLAY_H
 #define MCT_SNAKE_BOARDDISPLAY_H
 
+#include <cstdlib>
 
+class BoardDisplay {
+private:
+	int val;
+public:
+	BoardDisplay();
+	void init();
+};
 
 #endif //MCT_SNAKE_BOARDDISPLAY_H
