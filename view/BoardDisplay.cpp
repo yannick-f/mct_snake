772a65c798d1f7260f83dfdfa48befac497acc8d
freischladyannick <freischladyannick@gmail.com>
2021-07-02 13:57:11 +0200

    ADD BoardDisplay

---
+++ view/BoardDisplay.cpp
@@ -0,0 +1,5 @@
+//
+// Created by freischlad on 02.07.2021.
+//
+
+#include "BoardDisplay.h"
---
+++ view/BoardDisplay.h
@@ -0,0 +1,10 @@
+//
+// Created by freischlad on 02.07.2021.
+//
+
+#ifndef MCT_SNAKE_BOARDDISPLAY_H
+#define MCT_SNAKE_BOARDDISPLAY_H
+
+
+
+#endif //MCT_SNAKE_BOARDDISPLAY_H
