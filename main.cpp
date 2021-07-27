80d8d555591a1545a392d8a91c7aea31a21e4e9d
Yannick Freischlad <freischladyannick@gmail.com>
2021-07-27 21:33:34 +0200

    add button and navigation of startdisplay

--- main.cpp
+++ main.cpp
@@ -1,14 +1,8 @@
 #include "logic/Board.h"
 #include "gpio_msp432.h"
 #include "adc14_msp432.h"
-
-namespace DIFFICULTY {
-    int EASY    =   1;
-    int MEDIUM  =   2;
-    int HARD    =   3;
-    int EXTREME =   4;
-};
-
+#include "view/Display.h"
+#include "logic/enums.h"
 
 DIRECTION get_dir(int x_val, int y_val, int x_offset, int y_offset) {
 	if (x_val >= y_val) {
@@ -28,6 +22,10 @@ DIRECTION get_dir(int x_val, int y_val, int x_offset, int y_offset) {
 }
 
 int main(void) {
+	// initialise button
+	gpio_msp432_pin button(PORT_PIN(5, 1));
+	button.gpioMode(GPIO::INPUT | GPIO::PULLUP);
+
 	// initialise joystick
 	adc14_msp432_channel joy_x(15);
 	adc14_msp432_channel joy_y(9);
@@ -37,6 +35,29 @@ int main(void) {
 	uint16_t offset_x = joy_x.adcReadRaw();
 	uint16_t offset_y = joy_y.adcReadRaw();
 
+	// setup menu
+	Display display;
+	uGUI gui = display.init();
+	display.setStartDisplay(gui);
+
+	while (true) {
+		uint16_t read_x = joy_x.adcReadRaw();
+		uint16_t read_y = joy_y.adcReadRaw();
+
+		DIRECTION dir = get_dir(read_x, read_y, offset_x, offset_y);
+		display.change_pos(dir);
+
+		// check if button is pressed
+		if (button.gpioRead()) {
+			if (display.get_pos() == 3) {
+				break;
+			}
+		}
+	}
+
+	int difficulty = display.get_difficulty();
+
+	//game
 	Board board = Board();
 	while (!board.gameover) {
 		uint16_t read_x = joy_x.adcReadRaw();
