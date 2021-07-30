69cdc7c86f19b5bdbb2ab8e197a2059b4415bba1
Yannick Freischlad <freischladyannick@gmail.com>
2021-07-30 15:16:27 +0200

    game is ready

--- main.cpp
+++ main.cpp
@@ -27,6 +27,35 @@ DIRECTION get_dir(int x_val, int y_val, int x_offset, int y_offset) {
 	return REMAINS_SAME;
 }
 
+void run_game(Display display, uGUI gui) {
+
+	// initialise joystick
+	adc14_msp432_channel joy_x(15);
+	adc14_msp432_channel joy_y(9);
+	joy_x.adcMode(ADC::ADC_10_BIT);
+	joy_y.adcMode(ADC::ADC_10_BIT);
+
+	uint16_t offset_x = joy_x.adcReadRaw();
+	uint16_t offset_y = joy_y.adcReadRaw();
+
+	Board board = Board();
+	display.update_playdisplay(gui, board.get_board());
+
+	while (!board.get_gamestatus()) {
+		uint16_t read_x = joy_x.adcReadRaw();
+		uint16_t read_y = joy_y.adcReadRaw();
+
+		DIRECTION dir = get_dir(read_x, read_y, offset_x, offset_y);
+
+		board.make_move(dir);
+		display.update_playdisplay(gui, board.get_board());
+		task::sleep(500);
+	}
+	int score = board.get_score();
+	display.show_score(gui, score);
+	return;
+}
+
 int main(void) {
 	// Setup SPI interface
 	gpio_msp432_pin lcd_cs(PORT_PIN(5, 0));
@@ -47,51 +76,23 @@ int main(void) {
 	gpio_msp432_pin button(PORT_PIN(5, 1));
 	button.gpioMode(GPIO::INPUT | GPIO::PULLUP);
 
-	// initialise joystick
-	adc14_msp432_channel joy_x(15);
-	adc14_msp432_channel joy_y(9);
-	joy_x.adcMode(ADC::ADC_10_BIT);
-	joy_y.adcMode(ADC::ADC_10_BIT);
-
-	uint16_t offset_x = joy_x.adcReadRaw();
-	uint16_t offset_y = joy_y.adcReadRaw();
-
 	// setup menu
 	Display display;
-//	display.setStartDisplay(gui);
-//
-//	while (true) {
-//		uint16_t read_x = joy_x.adcReadRaw();
-//		uint16_t read_y = joy_y.adcReadRaw();
-//
-//		DIRECTION dir = get_dir(read_x, read_y, offset_x, offset_y);
-//		display.change_pos(dir);
-//
-//		// check if button is pressed
-//		if (button.gpioRead()) {
-//			if (display.get_pos() == 3) {
-//				break;
-//			}
-//		}
-//	}
-//
-//	int difficulty = display.get_difficulty();
+	display.setStartDisplay(gui);
 
-//game
-	Board board = Board();
-	display.update_playdisplay(gui, board.get_board());
-	while (!board.get_gamestatus()) {
-		uint16_t read_x = joy_x.adcReadRaw();
-		uint16_t read_y = joy_y.adcReadRaw();
-
-		DIRECTION dir = get_dir(read_x, read_y, offset_x, offset_y);
-
-		board.make_move(dir);
-		display.update_playdisplay(gui, board.get_board());
-		task::sleep(500);
+	while (button.gpioRead()) {
+		task::sleep(100);
 	}
-	int score = board.get_score();
-	display.show_score(gui, score);
+
+	//game
+	run_game(display, gui);
+
+	while(true) {
+		while (button.gpioRead()) {
+				task::sleep(100);
+			}
+			run_game(display, gui);
+	}
 
 	return 0;
 }
